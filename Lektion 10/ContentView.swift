15cbcb04a0ab213b537f8d4351cc9de3a260e896
Maurifisch <info@fischbein.de>
2023-05-07 18:12:52 +0200

    Initial Commit

---
+++ Lektion 10/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lektion 10
+//
+//  Created by Mauri Fischbein on 07.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lektion 10/Lektion_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Lektion_10App.swift
+//  Lektion 10
+//
+//  Created by Mauri Fischbein on 07.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Lektion_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
